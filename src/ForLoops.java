38846c36930522924f416c87aa07f64f696c44c1
Evan Zufah <cezufah@gmail.com>
2022-10-22 14:37:38 -0700

    .

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -2,14 +2,48 @@ import java.util.Scanner;
 
 public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
+    static int i = 0;
 
     public static void forSum() {
+        System.out.println("number?");
+        int n = s.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++){
+            sum = sum + i;
+        }
+        System.out.println("the sum is " + sum);
     }
 
     public static void rangeSum() {
+        System.out.println("number?");
+        int n = s.nextInt();
+        System.out.println("number?");
+        int n1 = s.nextInt();
+        int x = 0;
+        int y = 0;
+        i = 0;
+        while (i < n){
+            i++;
+            x = x + i;
+        }
+        int num = i;
+        while (i < n1){
+            i++;
+            y = y + i;
+        }
+        System.out.println("range is " + (Math.abs(x - y) + Math.min(num,i)));
     }
 
     public static void factorial() {
+        System.out.println("number?");
+        int n = 1;
+        int num = s.nextInt();
+        i = 0;
+        while (i < num){
+            i++;
+            n = n * i;
+        }
+        System.out.println("factorial: " + n);
     }
 
     public static void main(String[] args) {
--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -2,11 +2,28 @@ import java.util.Scanner;
 
 public class WhileLoops {
     private final static Scanner s = new Scanner(System.in);
+    static int total = 0;
+    static int even = 0;
+    static int odd = 0;
+    static int i =0;
 
     public static void eoCount() {
+        System.out.println("Enter numbers to determine whether they're odd or even. Enter 0 to generate the mean of all entered numbers.");
+        while ((i = s.nextInt()) != 0){
+            if (i % 2 == 0){
+                total = i + total;
+                even++;
+                System.out.println("Even");
+            } else {
+                total = i + total;
+                odd++;
+                System.out.println("Odd");
+            }
+        }
     }
 
     public static void mean() {
+        System.out.println("Mean = " + total / (even + odd));
     }
 
     public static void main(String[] args) {
